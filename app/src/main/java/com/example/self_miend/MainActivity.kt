45a2a5b5c9bf2642285799664c255d09518980e8
lueencee <lurence.pamoraga15@gmail.com>
2023-11-23 16:46:06 +0800

    finalers

--- app/src/main/java/com/example/self_miend/MainActivity.kt
+++ app/src/main/java/com/example/self_miend/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.self_miend
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val startingButton : Button = findViewById(R.id.startButton)
+        startingButton.setOnClickListener {
+            val aboutFragment = AboutFragment()
+            val fragment : Fragment? =
+
+            supportFragmentManager.findFragmentByTag(AboutFragment::class.java.simpleName)
+
+            if (fragment !is AboutFragment) {
+                supportFragmentManager.beginTransaction()
+                    .add(R.id.activityMainLayout, aboutFragment, AboutFragment::class.java.simpleName)
+                    .commit()
+            }
+            startingButton.visibility = View.GONE
+        }
     }
 }
