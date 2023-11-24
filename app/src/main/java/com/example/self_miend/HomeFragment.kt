cb99192bd4e321069ade4588b37b52cdf1d7c5a9
lueencee <lurence.pamoraga15@gmail.com>
2023-11-24 16:15:38 +0800

    finalers

--- app/src/main/java/com/example/self_miend/HomeFragment.kt
+++ app/src/main/java/com/example/self_miend/HomeFragment.kt
@@ -5,25 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val openTestsButton = view.findViewById<Button>(R.id.openDiffTestsButton)
+        openTestsButton.setOnClickListener {
+            val selectTestFragment = SelectTestFragment()
+            val transactionSelectTestFragment: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transactionSelectTestFragment.replace(R.id.activityMainLayout, selectTestFragment,)
+            transactionSelectTestFragment.commit()
+
+            openTestsButton.visibility = View.GONE
+        }
+
+        val openActButton = view.findViewById<Button>(R.id.openActivitiesButton)
+        openActButton.setOnClickListener {
+            val activitiesToTryFragment = SelectTestFragment()
+            val transactionActToTryFragment: FragmentTransaction = requireFragmentManager().beginTransaction()
+            transactionActToTryFragment.replace(R.id.activityMainLayout, activitiesToTryFragment,)
+            transactionActToTryFragment.commit()
+
+            openActButton.visibility = View.GONE
+        }
+
+        return view
+    }
 }
