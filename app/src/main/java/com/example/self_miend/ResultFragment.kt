f174e4edf6ea9fb3dc9affac7003474c65cdc2b8
lueencee <lurence.pamoraga15@gmail.com>
2023-11-28 00:07:26 +0800

    finalers

--- app/src/main/java/com/example/self_miend/ResultFragment.kt
+++ app/src/main/java/com/example/self_miend/ResultFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.FragmentTransaction
 
 class ResultFragment : Fragment() {
 
@@ -12,8 +14,37 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        val view = inflater.inflate(R.layout.fragment_result, container, false)
+
+        val doneButton = view.findViewById<Button>(R.id.resultDoneButton)
+        doneButton.setOnClickListener {
+            val resultDoneFragment = ResultDoneFragment()
+            val transactionSelectTestFragment: FragmentTransaction =
+                requireFragmentManager().beginTransaction()
+            transactionSelectTestFragment.replace(R.id.activityMainLayout, resultDoneFragment,)
+            transactionSelectTestFragment.commit()
+
+            doneButton.visibility = View.GONE
+        }
+
+        //Hotline Call
+
+        /*
+        val view = inflater.inflate(R.layout.fragment_result, container, false)
+
+        val hotlineButton = view.findViewById<Button>(R.id.hotlineCall)
+        hotlineButton.setOnClickListener {
+            val resultDoneFragment = ResultDoneFragment()
+            val transactionSelectTestFragment: FragmentTransaction =
+                requireFragmentManager().beginTransaction()
+            transactionSelectTestFragment.replace(R.id.activityMainLayout, resultDoneFragment,)
+            transactionSelectTestFragment.commit()
+
+            hotlineButton.visibility = View.GONE
+
+        } */
+
+        return view
     }
 
 }
